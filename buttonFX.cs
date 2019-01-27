a6e093ba23e6d01bcc7e0207c6caad7714747a99
rakesh-nori <20rakeshn@students.harker.org>
2019-01-26 23:31:04 -0800

    Add files via upload

---
+++ buttonFX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonFX : MonoBehaviour {
+
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+    public Button curr;
+
+	public void HoverSound()
+    {
+        if (curr.interactable)
+        {
+            myFx.PlayOneShot(hoverFx);
+        }
+    }
+
+    public void ClickSound()
+    {
+        if (curr.interactable)
+        {
+            myFx.PlayOneShot(clickFx);
+        }
+    }
+}
